effff4821a3ec2e113ee7d76b71714305eac75ff
TsigeredaTek <rose_ym@yahoo.com>
2020-11-09 13:29:52 -0600

    first

---
+++ src/server/routes/api/chirps.ts
@@ -0,0 +1,40 @@
+import * as express from 'express';
+import * as Chirps from '../../utilities/chirpstore'
+
+const router = express.Router();
+
+router.get('/:id?', (req, res, next) => {
+    let id = req.params.id;
+    if(id) {
+        res.send(Chirps.GetChirp(id))
+    } else {
+    res.send(Chirps.GetChirps());
+    }
+});
+
+
+
+router.post('/', (req, res, next) => {
+    let chirp = req.body;
+    Chirps.CreateChirp(chirp);
+    res.send(Chirps.GetChirps());
+    res.sendStatus(201);
+});
+
+router.delete('/:id', (req, res) => {
+    let id = req.params.id
+    Chirps.DeleteChirp(id);
+    res.send(Chirps.GetChirps());
+});
+
+router.put('/:id', (req, res) => {
+    let id = req.params.id;
+    let chirp = req.body;
+    Chirps.UpdateChirp(id, chirp);
+    res.send(Chirps.GetChirps());
+})
+
+export default router;
+
+
+
